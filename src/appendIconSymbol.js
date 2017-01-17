e3ad510c1e423c79cd4cd1f2a2d0a669119b6542
Robin Engström <robin.engstrom@nordnet.se>
2017-01-17 09:54:58 +0100

    Fix problem with IE not parsing XML due to faulty innerHTML polyfill

--- src/appendIconSymbol.js
+++ src/appendIconSymbol.js
@@ -1,17 +1,23 @@
-// Only polyfill SVG.innerHTML if needed
-// SVGElement not implemented in jsdom - https://github.com/tmpvar/jsdom/issues/1423
-if (!(typeof window === 'undefined') && !(typeof SVGElement === 'undefined') && !('innerHTML' in SVGElement.prototype)) {
-  require('innersvg-polyfill');
-}
+
+const isProbablyIE = !(typeof window === 'undefined') && !(typeof SVGElement === 'undefined') && !('innerHTML' in SVGElement.prototype);
 
 function appendIconSymbol(id, icon, spriteId) {
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.innerHTML = icon;
+  if (isProbablyIE) {
+    const svgContainer = document.createElement('div');
+    svgContainer.innerHTML = `<svg>${icon}</svg>`;
+    const svg = svgContainer.firstChild;
+    addToSprite(id, svg, spriteId);
+  } else {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.innerHTML = icon;
+    addToSprite(id, svg, spriteId);
+  }
+}
 
+function addToSprite(id, svg, spriteId) {
   const symbol = svg.firstChild;
   symbol.setAttribute('id', id);
   symbol.removeAttribute('xmlns'); // TODO: This should probably be done in the loader
-
   document.querySelector(`svg#${ spriteId }`).appendChild(symbol);
 }
 
